f66214ee24da213cb340f9713a58751650193ed6
Amin NAIRI <nairi.amin@gmail.com>
2018-09-15 14:33:14 +0200

    :memo: comments

--- src/js/rollup-plugin-generate-html.js
+++ src/js/rollup-plugin-generate-html.js
@@ -3,8 +3,13 @@ import { writeFile }  from 'fs'
 
 const pretty = require('pretty')
 
+// will grab the directory out of a full/relative path
 const baseDirectory   = file => file.slice(0, file.lastIndexOf('/'))
+
+// will transform a file path to it's script tag representation
 const fileToScript    = file => `<script src='./${file}'></script>`
+
+// will ensure that all bundle scripts are translated to script tags
 const scriptTagsFrom  = bundle => Object.keys(bundle).map(fileToScript).join('')
 
 export const generateHtml = () => ({
