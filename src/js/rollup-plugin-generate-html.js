fc2d186c76b98149d449b31f00d7301c183ecc15
Amin NAIRI <nairi.amin@gmail.com>
2018-09-15 15:13:36 +0200

    :sparkles: test

--- src/js/rollup-plugin-generate-html.js
+++ src/js/rollup-plugin-generate-html.js
@@ -1,6 +1,5 @@
-import { resolve }    from 'path'
-import { writeFile }  from 'fs'
-
+//import path from 'path'
+import fs from 'fs'
 
 const pretty = require('pretty')
 
@@ -15,15 +14,16 @@ const scriptTagsFrom  = bundle => Object.keys(bundle).map(fileToScript).join('')
 
 export const generateHtml = () => ({
   generateBundle({ file }, bundle) {
-    const path      = resolve(__dirname, baseDirectory(file), 'index.html')
+    //const outputPath      = path.resolve(__dirname, baseDirectory(file), 'index.html')
     const data      = pretty(`<!doctype html><head><meta charset='utf-8'><title>My Application</title></head><html><body>${scriptTagsFrom(bundle)}</body></html>`, { ocd: true })
     const encoding  = 'utf-8'
 
-    writeFile(path, data, 'utf-8', error => {
+    fs.writeFile('../dist/', 'hello', 'utf-8', error => {
       if (error) {
         this.error(error)
       }
     })
-    console.log('test 2')
+
+    console.log('test 3')
   }
 })
