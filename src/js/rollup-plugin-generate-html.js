fa9d2051937c53eef0f6110d9199dc797b40224c
Amin NAIRI <nairi.amin@gmail.com>
2018-09-15 15:38:00 +0200

    :sparkles: object destructuring on path & fs

--- src/js/rollup-plugin-generate-html.js
+++ src/js/rollup-plugin-generate-html.js
@@ -1,5 +1,5 @@
-import path from 'path'
-import fs from 'fs'
+import { resolve } from 'path'
+import { writeFile } from 'fs'
 
 const pretty = require('pretty')
 
@@ -14,12 +14,10 @@ const scriptTagsFrom  = bundle => Object.keys(bundle).map(fileToScript).join('')
 
 export const generateHtml = () => ({
   generateBundle({ file }, bundle) {
-    const outputPath = path.resolve(baseDirectory(file), 'index.html')
+    const path      = resolve(baseDirectory(file), 'index.html')
     const data      = pretty(`<!doctype html><head><meta charset='utf-8'><title>My Application</title></head><html><body>${scriptTagsFrom(bundle)}</body></html>`, { ocd: true })
     const encoding  = 'utf-8'
 
-    fs.writeFile(outputPath, data, encoding, error => error && this.error(error))
-
-    console.log('test 4')
+    writeFile(path, data, encoding, error => error && this.error(error))
   }
 })
