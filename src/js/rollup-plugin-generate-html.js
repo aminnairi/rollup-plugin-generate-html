5e565274cef5d15a19b627e7e389df91a77f08fb
Amin NAIRI <nairi.amin@gmail.com>
2018-09-15 15:34:19 +0200

    :bug: fixed the html generation

--- src/js/rollup-plugin-generate-html.js
+++ src/js/rollup-plugin-generate-html.js
@@ -15,10 +15,10 @@ const scriptTagsFrom  = bundle => Object.keys(bundle).map(fileToScript).join('')
 export const generateHtml = () => ({
   generateBundle({ file }, bundle) {
     const outputPath = path.resolve(baseDirectory(file), 'index.html')
-    console.log(outputPath)
-    // const outputPath      = path.resolve(__dirname, baseDirectory(file), 'index.html')
-    // const data      = pretty(`<!doctype html><head><meta charset='utf-8'><title>My Application</title></head><html><body>${scriptTagsFrom(bundle)}</body></html>`, { ocd: true })
-    // const encoding  = 'utf-8'
+    const data      = pretty(`<!doctype html><head><meta charset='utf-8'><title>My Application</title></head><html><body>${scriptTagsFrom(bundle)}</body></html>`, { ocd: true })
+    const encoding  = 'utf-8'
+
+    fs.writeFile(outputPath, data, encoding, error => error && this.error(error))
 
     console.log('test 4')
   }
