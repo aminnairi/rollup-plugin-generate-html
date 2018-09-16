624f18c31db1cb6fd3462466350b74730f78373f
Amin NAIRI <nairi.amin@gmail.com>
2018-09-16 15:26:41 +0200

    :sparkles: added the public path option

--- src/js/rollup-plugin-generate-html.mjs
+++ src/js/rollup-plugin-generate-html.mjs
@@ -12,11 +12,12 @@ const pretty = require('pretty')
  * @param {string} [configuration.title=My Application] The string used in the HTML title tag
  * @param {string} [configuration.lang=en-US] The string used in the lang attribute of the html tag
  * @param {string} [configuration.path] The destination where to put the HTML file
+ * @param {string} [configuration.publicPath=./] The string added before the script url
  */
-export const generateHtml = ({ charset = 'utf-8', title = 'My Application', lang = 'en-US', path } = {}) => ({
+export const generateHtml = ({ charset = 'utf-8', title = 'My Application', lang = 'en-US', path, publicPath = './' } = {}) => ({
   generateBundle({ file }, bundle) {
     const output    = resolve(path || baseDirectory(file), 'index.html')
-    const data      = pretty(`<!doctype html><html lang='${ lang }'><head><meta charset='${ charset }'><title>${ title }</title></head><body>${ scriptTagsFrom(bundle) }</body></html>`, { ocd: true })
+    const data      = pretty(`<!doctype html><html lang='${ lang }'><head><meta charset='${ charset }'><title>${ title }</title></head><body>${ scriptTagsFrom(bundle, publicPath) }</body></html>`, { ocd: true })
     const encoding  = 'utf-8'
 
     writeFile(output, data, encoding, error => error && this.error(error))
