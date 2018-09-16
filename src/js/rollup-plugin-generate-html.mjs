4da2d8da344b5da7d2da9d32bad52a107786dea8
Amin NAIRI <nairi.amin@gmail.com>
2018-09-16 11:58:06 +0200

    :sparkles: new title option + JSDoc

--- src/js/rollup-plugin-generate-html.mjs
+++ src/js/rollup-plugin-generate-html.mjs
@@ -5,10 +5,16 @@ import scriptTagsFrom from './scriptTagsFrom'
 
 const pretty = require('pretty')
 
-export const generateHtml = ({ charset = 'utf-8' } = {}) => ({
+/**
+ * @description Inject a bundle into an automatically created HTML file
+ * @param {object} configuration The configuration object
+ * @param {string} [configuration.charset=utf-8] The character set used in the meta charset tag
+ * @param {string} [configuration.title=My Application] The string used in the HTML title tag
+ */
+export const generateHtml = ({ charset = 'utf-8', title = 'My Application' } = {}) => ({
   generateBundle({ file }, bundle) {
     const path      = resolve(baseDirectory(file), 'index.html')
-    const data      = pretty(`<!doctype html><head><meta charset='${ charset }'><title>My Application</title></head><html><body>${ scriptTagsFrom(bundle) }</body></html>`, { ocd: true })
+    const data      = pretty(`<!doctype html><head><meta charset='${ charset }'><title>${ title }</title></head><html><body>${ scriptTagsFrom(bundle) }</body></html>`, { ocd: true })
     const encoding  = 'utf-8'
 
     writeFile(path, data, encoding, error => error && this.error(error))
