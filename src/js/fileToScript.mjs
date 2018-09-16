60155bcf527f3bbb65efaed0617479e384691537
Amin NAIRI <nairi.amin@gmail.com>
2018-09-16 15:25:57 +0200

    :sparkles: added public path argument

--- src/js/fileToScript.mjs
+++ src/js/fileToScript.mjs
@@ -1,2 +1,2 @@
 // will transform a file path to it's script tag representation
-export default file => `<script src='./${file}'></script>`
+export default publicPath => file => `<script src='${ publicPath }${ file }'></script>`
