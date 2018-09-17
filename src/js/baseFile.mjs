83cbe07dc40ae38ab4b459bae13bb2df92ebd3fd
Amin NAIRI <nairi.amin@gmail.com>
2018-09-17 20:48:46 +0200

    :memo: jsdoc

--- src/js/baseFile.mjs
+++ src/js/baseFile.mjs
@@ -1,3 +1,9 @@
+/**
+ * @description Get the path to the file without the first directory (if it exists)
+ * @param {string} file
+ */
 export default function baseFile(file) {
+
   return file.replace( /^(\.\.\/|\.\/|\/)?(.*?\/)/, '' )
+
 }
