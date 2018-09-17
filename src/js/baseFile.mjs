66172e5731cdc944a6ae69f0b21f3b3bf190eb7f
Amin NAIRI <nairi.amin@gmail.com>
2018-09-17 20:49:07 +0200

    :memo: jsdoc return

--- src/js/baseFile.mjs
+++ src/js/baseFile.mjs
@@ -1,6 +1,7 @@
 /**
  * @description Get the path to the file without the first directory (if it exists)
  * @param {string} file
+ * @return {string}
  */
 export default function baseFile(file) {
 
