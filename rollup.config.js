12feb7ca9879a939b10a45ec48384ab76003c466
Amin NAIRI <nairi.amin@gmail.com>
2018-09-22 09:55:10 +0200

    :wrench: configuration for license rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import uglify   from 'rollup-plugin-uglify-es'
 import resolve  from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import clean    from 'rollup-plugin-cleaner'
+import license from 'rollup-plugin-license'
 
 export default {
   // file to be bundled
@@ -33,6 +34,9 @@ export default {
     babel(),
 
     // will minify the output bundle
-    uglify()
+    uglify(),
+
+    // licensing the bundles
+    license({ banner: 'Copyright (c) 2018 Amin NAIRI' })
   ]
 }
