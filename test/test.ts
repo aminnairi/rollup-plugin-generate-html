ca6ea56dce976192c8a7b6f8316e16a9ed5df253
Amin NAIRI <nairi.amin@gmail.com>
2018-09-23 16:19:23 +0200

    :white_check_mark: added tests for baseFile

--- test/test.ts
+++ test/test.ts
@@ -1,29 +1,75 @@
 import { expect } from 'chai'
+
 import baseDirectory from '../src/baseDirectory'
+import baseFile from '../src/baseFile'
 
-describe('baseDirectory', function(): void {
+describe('rollup-plugin-generate-html', function(): void {
 
-  it('should return the directory name', function(): void {
+  describe('baseDirectory', function(): void {
 
-    expect(baseDirectory('dist/index.js')).to.be.equal('dist')
+    it('should return the directory name', function(): void {
+
+      expect(baseDirectory('dist/index.js')).to.be.equal('dist')
+
+    })
+
+    it('should return the directory name without the leading dot slash', function(): void {
+
+      expect(baseDirectory('./dist/index.js')).to.be.equal('dist')
+
+    })
+
+    it('should return the directory name without the subdirectory', function(): void {
+
+      expect(baseDirectory('./dist/js/index.js')).to.be.equal('dist')
+
+    })
+
+    it('should return the directory name with the trailing slash', function(): void {
+
+      expect(baseDirectory('/dist/index.js')).to.be.equal('/dist')
+
+    })
 
   })
 
-  it('should return the directory name without the leading dot slash', function(): void {
+  describe('baseFile', function(): void {
 
-    expect(baseDirectory('./dist/index.js')).to.be.equal('dist')
+    it('should return the file name when passing a there is a directory in the path', function(): void {
 
-  })
+      expect(baseFile('dist/index.js')).to.be.equal('index.js')
 
-  it('should return the directory name without the subdirectory', function(): void {
+    })
 
-    expect(baseDirectory('./dist/js/index.js')).to.be.equal('dist')
+    it('should return the file name when passing a there is a dot-slash in the path to the directory', function(): void {
 
-  })
+      expect(baseFile('./dist/index.js')).to.be.equal('index.js')
 
-  it('should return the directory name with the trailing slash', function(): void {
+    })
 
-    expect(baseDirectory('/dist/index.js')).to.be.equal('/dist')
+    it('should return the file name when passing a there is a slash in the path to the directory', function(): void {
+
+      expect(baseFile('/dist/index.js')).to.be.equal('index.js')
+
+    })
+
+    it('should return the file name with its subdirectory when passing a there is a directory in the path', function(): void {
+
+      expect(baseFile('dist/js/index.js')).to.be.equal('js/index.js')
+
+    })
+
+    it('should return the file name with its subdirectory when passing a there is a dot-slash in the path to the directory', function(): void {
+
+      expect(baseFile('./dist/js/index.js')).to.be.equal('js/index.js')
+
+    })
+
+    it('should return the file name with its subdirectory when passing a there is a slash in the path to the directory', function(): void {
+
+      expect(baseFile('/dist/js/index.js')).to.be.equal('js/index.js')
+
+    })
 
   })
 
