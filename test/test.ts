cdc6f879207a22b24fe1b542610165de37ab60c8
Amin NAIRI <nairi.amin@gmail.com>
2018-09-23 16:09:36 +0200

    :white_check_mark: added tests for baseDirectory

--- test/test.ts
+++ test/test.ts
@@ -1,10 +1,29 @@
 import { expect } from 'chai'
+import baseDirectory from '../src/baseDirectory'
 
-describe('test', function(): void {
+describe('baseDirectory', function(): void {
 
-  it('should be a test', function(): void {
+  it('should return the directory name', function(): void {
 
-    expect(true).to.be.equal(true)
+    expect(baseDirectory('dist/index.js')).to.be.equal('dist')
+
+  })
+
+  it('should return the directory name without the leading dot slash', function(): void {
+
+    expect(baseDirectory('./dist/index.js')).to.be.equal('dist')
+
+  })
+
+  it('should return the directory name without the subdirectory', function(): void {
+
+    expect(baseDirectory('./dist/js/index.js')).to.be.equal('dist')
+
+  })
+
+  it('should return the directory name with the trailing slash', function(): void {
+
+    expect(baseDirectory('/dist/index.js')).to.be.equal('/dist')
 
   })
 
