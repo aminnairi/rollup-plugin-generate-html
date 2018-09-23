4ece922b180c90ffbfb3ebee08faee1332255ecc
Amin NAIRI <nairi.amin@gmail.com>
2018-09-23 09:34:43 +0200

    :package: production file

--- dist/rollup-plugin-generate-html.js
+++ dist/rollup-plugin-generate-html.js
@@ -1,5 +1,6 @@
 /**
- * Copyright (c) 2018 Amin NAIRI
+ * @copyright 2018 Amin NAIRI
+ * @license MIT
  */
 
 "use strict";function baseDirectory(e){void 0===e&&(e="");var t=(/\w/i.exec(e)||{index:0}).index,r=e.indexOf("/",t);return-1===r?e.slice(0,t):e.slice(0,r)}function baseFile(e){return e.replace(/^(\.\.\/|\.\/|\/)?(.*?\/)/,"")}function generateHtml(e){var t=e.charset,r=void 0===t?"utf-8":t,i=e.title,n=void 0===i?"My Application":i,a=e.lang,l=void 0===a?"en-US":a,o=e.path,c=void 0===o?"":o,s=e.publicPath,d=void 0===s?"./":s;return{generateBundle:function(e){var t=this,i=e.file,a=path.resolve(c||baseDirectory(i),"index.html"),o=require("pretty")("<!doctype html><html lang='"+l+"'><head><meta charset='"+r+"'><title>"+n+"</title></head><body><script src='"+path.join(d,baseFile(i))+"'><\/script></body></html>",{ocd:!0});fs.writeFile(a,o,"utf-8",function(e){return e&&t.error(e)})}}}Object.defineProperty(exports,"__esModule",{value:!0});var path=require("path"),fs=require("fs");exports.generateHtml=generateHtml;
--- dist/rollup-plugin-generate-html.mjs
+++ dist/rollup-plugin-generate-html.mjs
@@ -1,5 +1,6 @@
 /**
- * Copyright (c) 2018 Amin NAIRI
+ * @copyright 2018 Amin NAIRI
+ * @license MIT
  */
 
 function baseDirectory(e){void 0===e&&(e="");var t=(/\w/i.exec(e)||{index:0}).index,i=e.indexOf("/",t);return-1===i?e.slice(0,t):e.slice(0,i)}function baseFile(e){return e.replace(/^(\.\.\/|\.\/|\/)?(.*?\/)/,"")}function generateHtml(e){var t=e.charset,i=void 0===t?"utf-8":t,r=e.title,n=void 0===r?"My Application":r,l=e.lang,o=void 0===l?"en-US":l,a=e.path,c=void 0===a?"":a,d=e.publicPath,s=void 0===d?"./":d;return{generateBundle:function(e){var t=this,r=e.file,l=resolve(c||baseDirectory(r),"index.html"),a=require("pretty")("<!doctype html><html lang='"+o+"'><head><meta charset='"+i+"'><title>"+n+"</title></head><body><script src='"+join(s,baseFile(r))+"'><\/script></body></html>",{ocd:!0});writeFile(l,a,"utf-8",function(e){return e&&t.error(e)})}}}import{join,resolve}from"path";import{writeFile}from"fs";export{generateHtml};
