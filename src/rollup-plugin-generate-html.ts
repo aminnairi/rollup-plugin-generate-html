569224490a261ed008180a583b6bf6899d9e34e3
Amin NAIRI <nairi.amin@gmail.com>
2018-09-23 17:03:49 +0200

    :bug: fixed issue where the file was not accessible in the buildEnd cb

--- src/rollup-plugin-generate-html.ts
+++ src/rollup-plugin-generate-html.ts
@@ -48,7 +48,7 @@ export function generateHtml({ charset = 'utf-8', title = 'My Application', lang
   }
 
   return {
-    buildEnd({ file }: GeneratedBundleOptions): void {
+    generateBundle({ file }: GeneratedBundleOptions): void {
 
       if ( typeof file !== 'string' ) {
 
