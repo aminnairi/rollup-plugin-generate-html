db62af6a59fb6541d6743e9c95d2bb3d35239e28
Amin NAIRI <nairi.amin@gmail.com>
2018-09-23 10:08:47 +0200

    :sparkles: added runtime type checking for file argument

--- src/baseFile.ts
+++ src/baseFile.ts
@@ -1,4 +1,10 @@
-export default function baseFile(file: string): string {
+export default function baseFile( file: string ): string {
+
+  if ( typeof file !== 'string' ) {
+
+    throw TypeError( `first argument expected to be of type string, ${ typeof file } provided` )
+
+  }
 
   return file.replace( /^(\.\.\/|\.\/|\/)?(.*?\/)/, '' )
 
