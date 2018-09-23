4c90ef84882d16283f79d370c763b1f0b62efd26
Amin NAIRI <nairi.amin@gmail.com>
2018-09-23 09:31:23 +0200

    :tada: added definition file

---
+++ dist/rollup-plugin-generate-html.d.ts
@@ -0,0 +1,9 @@
+interface GenerateHtmlOptions {
+    charset?: string;
+    title?: string;
+    lang?: string;
+    path?: string;
+    publicPath?: string;
+}
+export declare function generateHtml({ charset, title, lang, path, publicPath }: GenerateHtmlOptions): Object;
+export {};
