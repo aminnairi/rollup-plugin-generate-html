7e3b72dd824c015c809d14d8ecfaaa536104328a
Amin NAIRI <nairi.amin@gmail.com>
2018-09-26 21:29:23 +0200

    :rotating_light: fixed tslint issues

--- src/baseFile.ts
+++ src/baseFile.ts
@@ -1,11 +1,11 @@
-export default function baseFile( file: string ): string {
+export const baseFile: (file: string) => string = (file: string): string => {
 
-  if ( typeof file !== 'string' ) {
+  if (typeof file !== 'string') {
 
-    throw TypeError( `first argument expected to be of type string, ${ typeof file } provided` )
+    throw TypeError(`first argument expected to be of type string, ${ typeof file } provided`)
 
   }
 
-  return file.replace( /^(\.\.\/|\.\/|\/)?(.*?\/)/, '' )
+  return file.replace(/^(\.\.\/|\.\/|\/)?(.*?\/)/, '')
 
 }
