1438accf8070e731c77f932a806910051d193d09
Amin NAIRI <nairi.amin@gmail.com>
2018-09-26 21:32:00 +0200

    :rotating_light: fixed tslint issues

--- src/baseDirectory.ts
+++ src/baseDirectory.ts
@@ -1,23 +1,23 @@
 import { normalize } from 'path'
 
-export default function( file: string = '' ): string {
+export const baseDirectory: (file?: string) => string = (file: string = ''): string => {
 
-  if ( typeof file !== 'string' ) {
+  if (typeof file !== 'string') {
 
-    throw TypeError( `first argument expected to be of type string, ${ typeof file } provided` )
-
-  }
-
-  const firstLetterIndex = (/\w/i.exec( file ) || { index: 0 }).index
-
-  const firstSeparatorIndex = file.indexOf( '/', firstLetterIndex )
-
-  if ( firstSeparatorIndex === -1 ) {
-
-    return normalize( file.slice( 0, firstLetterIndex ) )
+    throw TypeError(`first argument expected to be of type string, ${ typeof file } provided`)
 
   }
 
-  return normalize( file.slice( 0, firstSeparatorIndex ) )
+  const firstLetterIndex: number = (/\w/i.exec(file) || { index: 0 }).index
+
+  const firstSeparatorIndex: number = file.indexOf('/', firstLetterIndex)
+
+  if (firstSeparatorIndex === -1) {
+
+    return normalize(file.slice(0, firstLetterIndex))
+
+  }
+
+  return normalize(file.slice(0, firstSeparatorIndex))
 
 }
